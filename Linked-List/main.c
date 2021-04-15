c53d46eec054ea0e9fcbc533c9137fc4074aab2a
Nader Atef <naderatef13@gmail.com>
2021-04-15 23:02:56 +0200

    all functions are working with no syntax errors but not yet tested.

--- Linked-List/main.c
+++ Linked-List/main.c
@@ -6,6 +6,7 @@ int id;
 int date[3];
 int ly_result;
 };
+int n;
 void insert_begin(struct student* ptr, struct student obj1){
 int i;
 for(i=n-1;i>0;i--){
@@ -16,7 +17,8 @@ for(i=n-1;i>0;i--){
 void insert_end(struct student* ptr, struct student obj1){
 int insertion_index;
 for(int i=0;i<n;i++){
-  if(*(ptr+i)==0){
+
+  if((ptr+i)->id==0){
     insertion_index=i;
 
   }
@@ -25,21 +27,19 @@ for(int i=0;i<n;i++){
 *(ptr+insertion_index)=obj1;
 }
 void insert_middle(struct student *ptr,struct student obj2){
-if(n%2==0){
-    mid_index=(n/2)-1;
+int mid_index=(n-1)/2;
+int i;
+for(i=n-1;i>mid_index;i--){
+    *(ptr+i)=*(ptr+i-1);
 }
-else{
-
-    mid_index=n/2;
-
+*(ptr+mid_index)=obj2;
 }
-}
-int n;
 int main()
-{
+{ printf("please enter the number of students you will store \n");
 scanf("%d",&n);
 struct student *ptr_student=(struct student* )calloc(n,sizeof(struct student));
-
+struct student s1;
+insert_begin(ptr_student,s1);
 
 
     return 0;
